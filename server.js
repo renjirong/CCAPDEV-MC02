4b2128434b0bdb66eb2c70b7dee1ff39d037e3b8
renjirong <70014388+renjirong@users.noreply.github.com>
2021-01-10 13:44:08 +0800

    Added dependencies

--- server.js
+++ server.js
@@ -1,20 +1,7 @@
-var express = require('express');
-var path = require ('path');
-var exphbs = require ('express-handlebars');
+const express = require('express');
 
-var app = express ();
+const app = express();
 
-app.set('views', path.join(__dirname, 'views'));
-app.engine('handlebars', exphbs({defautLayout:'main'}));
-app.set('view engine', 'handlebars');
+const PORT = process.env.PORT || 6969;
 
-app.set('port' , (process.env.PORT || 6969));
-
-app.get('/', function(req, res){
-
-    res.render('home');
-})
-
-app.listen(app.get('port'), function (){
-    console.log('Server started on port '+ app.get('port'))
-}); 
+app.listen(PORT, console.log('Server started on port '+PORT));
